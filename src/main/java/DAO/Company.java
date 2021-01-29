8635f0bf8fe654efdb91a40688288507db0fed84
Dorota Miler <dorota.miler7@gmail.com>
2021-01-29 14:30:36 +0100

    cCompany class addition with necessary properties

---
+++ src/main/java/DAO/Company.java
@@ -0,0 +1,31 @@
+package DAO;
+
+public class Company {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
